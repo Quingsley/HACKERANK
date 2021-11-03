28b97bb61db053fcb7f508301629e353fe98d841
JEROME JUMAH <74877189+Quingsley@users.noreply.github.com>
2021-11-03 10:08:43 +0300

    Update array.cpp

--- HACKERANK/array.cpp
+++ HACKERANK/array.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 int main()
 {
-    int arr[5];
+    int n;
+    int arr[n];
     int j;
-    for(int i=0;i<5;i++)
+    cout << endl;
+    for(int i=0;i<n;i++)
         {
             cin >> arr[i];
             
@@ -13,7 +15,7 @@ int main()
     for(j=4;j>=0;j--)
         {
             
-            cout << arr[j] << endl;
+            cout << arr[j] << " ";
         }
     return 0;
 }
